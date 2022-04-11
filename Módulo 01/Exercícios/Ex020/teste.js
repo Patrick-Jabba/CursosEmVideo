fc1a8f2e3e40385ca3255c39ab915a094fe890c5
Patrick-Jabba <monteiromonterio@gmail.com>
2022-04-11 19:23:38 -0300

    desafios de js basico

---
+++ Módulo 01/Exercícios/Ex020/teste.js
@@ -0,0 +1,11 @@
+function myFunction (a, b){
+    if (a === b) {
+        return true;
+    } else return false;
+}
+
+console.log(myFunction(2, 3));
+console.log(myFunction(3, 3));
+console.log(myFunction(1, "1"));
+console.log(myFunction("10", "10"));
+
---
+++ Módulo 01/Exercícios/Ex020/teste3.js
@@ -0,0 +1,5 @@
+function myFunction(a, b, c, d, e, f){
+    return  (a + b);
+}
+
+console.log(myFunction(6, 5, 4, 3, 2, 1));
